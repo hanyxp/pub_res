983f01802543082271e842741b4c98ddb7eca18e
hanyue <hanyxp@163.com>
2016-06-07 20:08:19 +0800

    init~

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello World1");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
-        System.out.println("Hello git commit");
+        System.out.println("Hello git modify");
     }
 }
